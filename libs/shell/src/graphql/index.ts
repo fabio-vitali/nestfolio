7ae6a1a1787056bd6f8c688ac611b0997ba11955
fabio-vitali <fabio.vitali@gmail.com>
2026-04-26 17:45:35 +0200

    chore(b3): re-export factory + token + helper; delete APPSYNC_CONFIG
    @nestfolio/shell/graphql now exposes MFE_DOMAIN, provideMfeGraphql,
    createApolloClient (+ its options type). APPSYNC_CONFIG / AppSyncConfig
    deleted along with their unit test.

--- libs/shell/src/graphql/index.ts
+++ libs/shell/src/graphql/index.ts
@@ -1,4 +1,6 @@
-export type { AppSyncConfig } from './appsync.config';
-export { APPSYNC_CONFIG } from './appsync.config';
 export { GraphqlService } from './graphql.service';
 export { CachedQuery } from './cached-query';
+export { MFE_DOMAIN } from './mfe-domain.token';
+export { provideMfeGraphql } from './provide-mfe-graphql';
+export { createApolloClient } from './create-apollo-client';
+export type { CreateApolloClientOptions } from './create-apollo-client';
